02d7c4ff9465b840d2e3427ea968e68cde0c769c
Butter <havenkain@gmail.com>
2024-11-14 10:16:08 -0600

    added asteoid field

--- asteroid.py
+++ asteroid.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from circleshape import *
 
-class Asteroid:
-
+class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        pass
+        super().__init__(x, y, radius)
 
-    def draw(self):
-        pygame.draw.circle()
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
