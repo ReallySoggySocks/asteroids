4122b7e24c1e890b7392e328010dc3a8e2b97ce0
Butter <havenkain@gmail.com>
2024-11-14 09:46:32 -0600

    added player

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+class Asteroid:
+
+    def __init__(self, x, y, radius):
+        pass
+
+    def draw(self):
+        pygame.draw.circle()
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
 from constants import * 
+from player import *
 
 
 def main():
+    print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
+    dt = 0
 
     while True:
-        screen.fill(000)
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updateable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
